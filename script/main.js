0c30d24699264e6615ac8503523f33fd069b077c
Abdulrahmon Soliu <abdurrahmanabusaad7@gmail.com>
2024-06-23 18:18:12 +0100

    added more files and implemeted the product layout and functionality

--- script/main.js
+++ script/main.js
@@ -4,10 +4,10 @@ const increaseQuantity = document.querySelector(".plus");
 let productQuantity = document.querySelector(".zero");
 const addToCart = document.querySelector(".cart-setion");
 const cartIcon = document.querySelector(".cart-icon");
+const productDisplay = document.querySelector(".product-display");
 
 let updatedQuantity = 0;
 decreaseQuantity.addEventListener("click", () => {
-	console.log("added");
 	updatedQuantity -= 1;
 	productQuantity.innerHTML = updatedQuantity;
 	cartQuantity.innerHTML = updatedQuantity;
@@ -18,3 +18,25 @@ increaseQuantity.addEventListener("click", () => {
 	productQuantity.innerHTML = updatedQuantity;
 	cartQuantity.innerHTML = updatedQuantity;
 });
+
+let html = ``;
+products.forEach((product) => {
+	html += `<div class="product-container">
+	<img class="resize-img" src="${product.image}" alt="" />
+	<div class="product-name">${product.name}</div>
+	<div class="product-quantity_container">
+		<select class="product-selected">
+			<option selected value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+		</select>
+	</div>
+	<div class="product-price_container">Price : $${product.price.toFixed(2)} </div>
+	<div class="added-to-cart">Add to cart</div>
+</div>
+`;
+});
+
+productDisplay.innerHTML = html;
